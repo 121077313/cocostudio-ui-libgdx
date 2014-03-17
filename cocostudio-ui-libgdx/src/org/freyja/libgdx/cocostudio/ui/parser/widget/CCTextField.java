32cc7b0b7d4ed4c79ddad5fdf7f43066d656fe9b
isee <121077313@qq.com>
2014-03-17 16:34:23 +0800

    添加默认字符,否则无法输入英文数字等.

--- cocostudio-ui-libgdx/src/org/freyja/libgdx/cocostudio/ui/parser/widget/CCTextField.java
+++ cocostudio-ui-libgdx/src/org/freyja/libgdx/cocostudio/ui/parser/widget/CCTextField.java
@@ -11,6 +11,11 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle;
 
+/**
+ * 
+ * @author i see
+ * 
+ */
 public class CCTextField extends WidgetParser {
 
 	@Override
@@ -18,18 +23,27 @@ public class CCTextField extends WidgetParser {
 		return "TextField";
 	}
 
+	/** 默认文字,为了支持基本字符输入 */
+	final String defaultText = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890\"!`?'.,;:()[]{}<>|/@\\^$-%+=#_&~*";
+
 	@Override
 	public Actor parse(CocoStudioUIEditor editor, CCWidget widget,
 			CCOption option) {
 
 		final TTFLabelStyle labelStyle = editor.createLabelStyle(option);
+
 		TextFieldStyle style = new TextFieldStyle(labelStyle.font,
 				labelStyle.fontColor, null, null, null);
+
+		// style.font = FontUtil.createFont(
+		// labelStyle.getFontFileHandle(), "0123456789",
+		// labelStyle.getFontSize());
+
 		TextField textField = new TextField(option.getText(), style) {
 
 			@Override
 			public void setText(String text) {
-				String sumText = text + getMessageText();
+				String sumText = text + getMessageText() + defaultText;
 
 				getStyle().font = FontUtil.createFont(
 						labelStyle.getFontFileHandle(), sumText,
@@ -41,7 +55,7 @@ public class CCTextField extends WidgetParser {
 			@Override
 			public void setMessageText(String messageText) {
 
-				String sumText = messageText + getText();
+				String sumText = messageText + getText() + defaultText;
 
 				getStyle().font = FontUtil.createFont(
 						labelStyle.getFontFileHandle(), sumText,
@@ -49,6 +63,8 @@ public class CCTextField extends WidgetParser {
 				super.setMessageText(messageText);
 			};
 		};
+		
+		textField.setMaxLength(option.getMaxLength());
 		textField.setMessageText(option.getPlaceHolder());
 		textField.setPasswordMode(option.isPasswordEnable());
 		textField.setPasswordCharacter(option.getPasswordStyleText());
